3dcbe182d853d4466118b34464061aba00813efe
sunwenou <sunwenou@lrhealth.com>
2025-08-25 12:31:44 +0800

    修改路径

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("/data/blog.json");
+const router = jsonServer.router("data/blog.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
